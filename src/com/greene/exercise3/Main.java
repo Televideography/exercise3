2f6d1bdd784a6921fa0d407791fcbc3cb923518b
Televideography <thefact25@gmail.com>
2018-02-01 14:17:14 -0500

    Initial commit

---
+++ src/com/greene/exercise3/Main.java
@@ -0,0 +1,30 @@
+package com.greene.exercise3;
+import java.util.Scanner;
+public class Main {
+
+    static double temperature = 0;
+
+    static void userInput() {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter a temperature in Fahrenheit: ");
+        temperature = reader.nextDouble();
+    }
+
+    static void toCelsius() {
+        temperature = (temperature - 32) * 5 / 9;
+    }
+
+    static void toUser(){
+        System.out.println("In Celsius that's " + temperature);
+    }
+
+
+    public static void main(String[] args) {
+
+        while(temperature > -460){
+            userInput();
+            toCelsius();
+            toUser();
+        }
+    }
+    }
